bc7568bbc785e1f9c80358b4aa4e576c1d9d5a79
Andrew M <me@r3nya.ru>
2015-08-09 11:39:07 +0300

    Added BrowserSync

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 module.exports = {
   entry    : "./app/App.jsx",
   output   : {
@@ -11,5 +13,12 @@ module.exports = {
         loader   : 'babel'
       }
     ]
-  }
+  },
+  plugins  : [
+    new BrowserSyncPlugin({
+      host     : 'localhost',
+      port     : 3000,
+      server   : { baseDir: ['public'] }
+    })
+  ]
 };
